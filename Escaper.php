bd52964586daa5fa7f1649ff940c1e5f4cc3a554
Marco Pivetta <ocramius@gmail.com>
2014-06-23 19:54:06 +0000

    Importing state as of zendframework/zf2@2a9c97db0b21336d85937caaec2ecda3de6dc474 (1403553246)
    Automatic import via rsync

--- Escaper.php
+++ Escaper.php
@@ -3,12 +3,13 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Zend\Escaper;
 
+use Zend\Escaper\Exception;
 
 /**
  * Context specific methods for use in secure output escaping
@@ -368,6 +369,7 @@ class Escaper
      */
     protected function convertEncoding($string, $to, $from)
     {
+        $result = '';
         if (function_exists('iconv')) {
             $result = iconv($from, $to, $string);
         } elseif (function_exists('mb_convert_encoding')) {
